480b41eac665fa27143a82d1724e9f4715cb2f65
James Ireland <james.ireland@softwire.com>
2025-07-08 16:40:39 +0100

    cross origin enabled

--- src/main/java/org/softwire/training/bookish/controllers/BrowseController.java
+++ src/main/java/org/softwire/training/bookish/controllers/BrowseController.java
@@ -3,12 +3,14 @@ package org.softwire.training.bookish.controllers;
 import org.softwire.training.bookish.models.database.Book;
 import org.softwire.training.bookish.services.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/browse")
 public class BrowseController {
 
